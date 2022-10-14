0739f13a93bf518197f7f0d80e32dc5f414df8dd
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-10-14 22:47:17 +0300

    add process functions init

--- cmd/main.go
+++ cmd/main.go
@@ -4,26 +4,29 @@ import (
 	"errors"
 	"os"
 	"sync"
+
+	tools "github.com/ssergomol/data-viewer/pkg/process-tools"
 )
 
-func Process() {
+func Process(filePath string) {
 	// TODO: create reporter
+	reporter := tools.NewReporter(filePath)
 
 	// TODO: create parser
-
+	parser := tools.NewParser(filePath, reporter)
 	// TODO: create waitgropup
 
 	wg := new(sync.WaitGroup)
 	wg.Add(2)
 
 	// create channels for goroutine comminication
+	entries := make(chan []string)
+	done := make(chan bool)
 
-	entry := make(chan []string)
-	done := make(chan []bool)
+	converter := tools.NewConverter(filePath, reporter)
 
-	// TODO: run goroutine to convert data
-
-	// TODO: run goroutine to read csv data
+	go converter.ProcessEntry(wg, entries, done)
+	go parser.Read(wg, entries, done)
 
 	// wait untill all goroitines finish
 	wg.Wait()
@@ -47,5 +50,5 @@ func main() {
 		panic(err)
 	}
 
-	Process()
+	Process(fileName)
 }
