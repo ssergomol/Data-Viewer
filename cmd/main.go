f9aa745fe0e01b6a013b9c2e901faf3054a852bc
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-10-15 05:26:23 +0300

    add general documentation

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,22 @@
+/*
+This programm processes your "csv" or "prn" files and
+creates HTML representation. If it is csv file, then
+the table in HTML will be created, in other case of "prn" file - the
+simple text will be represented in HTML format
+
+Usage:
+
+	main.go [path] [delimeter]
+
+path:
+	Path to file with "csv" or "prn" extensions.
+
+delimters:
+	Delimeter might be set only if path leads to csv file.
+	By default the comma "," delimter is set, but you can define any
+	other delimeter except "\n" and "\r".
+*/
+
 package main
 
 import (
@@ -10,21 +29,18 @@ import (
 )
 
 func startProcessing(filePath string, delim rune, ext string) {
-	// TODO: create reporter
+	// Create information reporter, parser and converter for data processing
 	reporter := tools.CreateInfo(filePath, delim, ext)
-
-	// TODO: create parser
 	parser := tools.NewParser(filePath, reporter)
-	// TODO: create waitgropup
+	converter := tools.NewConverter(filePath, reporter)
 
+	// Create wait group to wait untill all goroutines end their execution
 	wg := new(sync.WaitGroup)
 	wg.Add(2)
 
-	// create channels for goroutine comminication
+	// create channels for goroutine communication
 	entries := make(chan []string)
 	done := make(chan bool)
-
-	converter := tools.NewConverter(filePath, reporter)
 
 	go converter.ProcessEntry(wg, entries, done)
 	go parser.Read(wg, entries, done)
@@ -34,25 +50,31 @@ func startProcessing(filePath string, delim rune, ext string) {
 }
 
 func main() {
+	// Check if the path to file was provided
 	if len(os.Args[1:]) == 0 {
 		err := errors.New("no filepathes were provided!")
 		panic(err)
 	}
 
+	// Get Inforamtion about the file
 	fileName := os.Args[1]
-
 	fileInfo, err := os.Stat(fileName)
 	if err != nil {
 		panic(err)
 	}
 
+	// Check if the file is not a directory
 	if fileInfo.IsDir() {
 		err := errors.New("can't read the directiory")
 		panic(err)
 	}
 
+	// Get the extension and provided delimeter
 	fileExt := filepath.Ext(fileName)
-
+	if fileExt != ".prn" && fileExt != ".csv" {
+		err := errors.New("Wrong file extension")
+		panic(err)
+	}
 	delimeter := ','
 	if len(os.Args[2:]) != 0 {
 		delimeter = rune(os.Args[2][0])
